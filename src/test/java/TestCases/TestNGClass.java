49304cef7ba528257978fc51dc87a8689ff5b0ab
rhaldar <rhaldar@parchment.com>
2018-09-15 11:02:07 -0700

    Added my very first page object for Mercury Tour Website

--- src/test/java/TestCases/TestNGClass.java
+++ src/test/java/TestCases/TestNGClass.java
@@ -1,24 +1,30 @@
 package TestCases;
 
+import PageObjects.MercuryToursHomePage;
+import com.sun.media.jfxmedia.logging.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TestNGClass {
 
-    @Test(priority = 1, description = "Student Login")
+    WebDriver driver;
+
+    @Test(priority = 1)
     public void loginApplication(){
-        System.out.println("This is my first test");
+
+        driver = new ChromeDriver();
+        driver.get("http://newtours.demoaut.com/mercurywelcome.php");
+        String currentURL = driver.getCurrentUrl();
+        Assert.assertTrue(currentURL.contains("/mercurywelcome.php"));
+        Logger.logMsg(1,"\"This is my first login application test\"");
+        MercuryToursHomePage mtHome = new MercuryToursHomePage();
+        mtHome.userName.sendKeys("tutorial");
+        mtHome.password.sendKeys("tutorial");
+        mtHome.loginButton.click();
+        driver.quit();
 
     }
 
-    @Test(priority = 2, description = "Student selecting their credentials")
-    public void selectItems(){
-        System.out.println("This is my second test");
-
-    }
-
-    @Test(priority = 3, description = "Student placing Transcript order")
-    public void checkoutItems(){
-        System.out.println("This is my third test");
-
-    }
 }
